1a3fb206304a74804cab8d0e4296c865d648fc5c
vesteller <127887428+vesteller@users.noreply.github.com>
2024-01-29 16:57:02 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,14 +13,32 @@ let miTitulo = document.querySelector( 'h1');
 
 function estableceNombreUsuario() {
     let miNombre = prompt('Introduzca su nombre.');
-    let a = parseInt(prompt('Introduzca el valor de A: '));
-    let b = parseInt(prompt('Introduzca el valor de B: '));
-    let c = a + b;
+    let a = parseInt(prompt('Introduzca su altura en cm: '));
+    let b = parseInt(prompt('Introduzca su peso en kg: '));
+    let imc = b / a;
+    if (imc < 16) {
+        let d = 'Infrapeso: Delgadez Severa';
+    } else if (16 <= imc and imc <= 16.99) {
+        let d = 'Infrapeso: Delgadez moderada';
+    } else if (17 <= imc and imc <= 18.49) {
+        let d = 'Infrapeso: Delgadez aceptable';
+    } else if (18.50 <= imc and imc <= 24.99) {
+        let d = 'Peso Normal';
+    } else if (25.00 <= imc and imc <= 29.99) {
+        let d = 'Sobrepeso';
+    } else if (30.00 <= imc and imc <= 34.99) {
+        let d = 'Obeso: Tipo I';
+    } else if (35.00 <= imc and imc <= 40.00) {
+        let d = 'Obeso: Tipo II';
+    } else if (imc > 40.00) {
+        let d = 'Obeso: Tipo III';
+    } 
+    
     if(!miNombre) {
       estableceNombreUsuario();
     } else {
       localStorage.setItem('nombre', miNombre);
-      miTitulo.innerHTML = 'Mozilla is FINO, ' + miNombre + ' A + B = ' + c;
+      miTitulo.innerHTML = 'Mozilla is FINO, ' + miNombre + ' de acuerdo a su imc ud es: ' + d;
     }
   }
   
