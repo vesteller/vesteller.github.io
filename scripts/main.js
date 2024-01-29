32a1eca428daeae33abd0b72e4acc0649dca47d7
vesteller <127887428+vesteller@users.noreply.github.com>
2024-01-29 16:35:18 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ function estableceNombreUsuario() {
     let miNombre = prompt('Introduzca su nombre.');
     let a = prompt('Introduzca el valor de A: ');
     let b = prompt('Introduzca el valor de B: ');
-    c = parseInt(a) + parseInt(b)
+    let c = (parseInt(a) + parseInt(b));
     if(!miNombre) {
       estableceNombreUsuario();
     } else {
